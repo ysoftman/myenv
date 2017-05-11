143614d99974389ceed9c5207ab620e6e03ddb78
ysoftman <ysoftman@gmail.com>
2017-05-11 16:06:21 +0900

    agnoster 테마 추가

--- installohmyzsh.zsh
+++ installohmyzsh.zsh
@@ -6,4 +6,6 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # 미리 설정한 template 파일을 복사
 # .zshrc 에서 ~/.oh-my-zsh/templates/zshrc.zsh-template 로드하여 사용
 cp -v zshrc.zsh-template ~/.oh-my-zsh/templates/zshrc.zsh-template
+# 미리 설정한 agnoster 테마 파일을 복사
+cp -v agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
