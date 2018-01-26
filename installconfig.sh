9142a84f6ad6e785973c084a09e9d9e5f9269a24
ysoftman <ysoftman@gmail.com>
2018-01-26 18:23:36 +0900

    gitignore 설정 추가

--- installconfig.sh
+++ installconfig.sh
@@ -9,10 +9,14 @@ git config --global color.ui auto
 git config --global core.editor vim
 git config --global merge.tool vimdiff
 
+# git ignore
+cp -fv .gitignore_global ~
+
+
 # bash, zsh, vim 기본 설정
-cp -fv ./.bashrc ~/.bashrc
-cp -fv ./.zshrc ~/.zshrc
-cp -fv ./.vimrc ~/.vimrc
+cp -fv .bashrc ~
+cp -fv .zshrc ~
+cp -fv .vimrc ~
 
 # 보안사항으로 실제 필요할때만 사용하자
 # cp -fv ./.ssh/* ~/.ssh
