7fcba9bfb998d189cc6ca6a0a7d5428d5b74fa89
ysoftman <ysoftman@gmail.com>
2018-01-31 19:46:45 +0900

    linux alias ls

--- .myenv.sh
+++ .myenv.sh
@@ -2,7 +2,6 @@
 export GOPATH=$HOME/workspace/gopath
 export PATH=$PATH:$HOME:$GOPATH/bin:
 export PATH=$PATH:/usr/local/opt/openssl/bin:
-
 export PYTHONPATH=/Library/Python/2.7/site-packages
 #export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/site-packages
 export EDITOR=vim
@@ -17,6 +16,7 @@ elif [[ $(uname) == 'Linux' ]]; then
     export LC_ALL=ko_KR.utf8
     export PS1="\u@\h:\w\$ "
     export LS_COLORS='no=00:fi=00:di=00;36:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:*.sh=00;32:'
+    alias ll='ls -ahl --color=auto'
 fi
 
 alias vi='vim'
