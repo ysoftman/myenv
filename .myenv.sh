36e3a7d2f86c8d5572666f1e372d7b428a9bed3c
ysoftman <ysoftman@gmail.com>
2018-03-14 20:36:11 +0900

    PYTHONPATH 설정

--- .myenv.sh
+++ .myenv.sh
@@ -8,7 +8,7 @@ export VISUAL=vim
 if [[ $(uname) == 'Darwin' ]]; then
     export LSCOLORS='GxFxCxDxBxegedabagaced'
     export CLICOLOR=1
-    #export PYTHONPATH=/Library/Python/2.7/site-packages
+    export PYTHONPATH=/Library/Python/2.7/site-packages
     alias ll='ls -ahlG'
     alias sn='pmset displaysleepnow'
 elif [[ $(uname) == 'Linux' ]]; then
@@ -16,7 +16,7 @@ elif [[ $(uname) == 'Linux' ]]; then
     export LC_ALL=ko_KR.utf8
     export PS1="\u@\h:\w\$ "
     export LS_COLORS='no=00:fi=00:di=00;36:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:*.sh=00;32:'
-    #export PYTHONPATH=/usr/local/lib/python2.7/site-packages
+    export PYTHONPATH=/usr/local/lib/python2.7/site-packages
     alias ll='ls -ahl --color=auto'
 fi
 
