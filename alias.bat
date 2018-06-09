4fd1c4790b9cbd1bec89b3f69769ee6ea71bd3d9
ysoftman <ysoftman@gmail.com>
2018-06-09 16:54:24 +0900

    add vim alias

--- alias.bat
+++ alias.bat
@@ -4,5 +4,6 @@ reg add "HKCU\Software\Microsoft\Command Processor" /v autorun /t reg_multi_sz /
 reg query "HKCU\Software\Microsoft\Command Processor" /v autorun
 
 @REM set alias(doskey)
+doskey vi=vim
 doskey work=cd /d d:\ysoftman\workspace
 doskey testcode=cd /d d:\ysoftman\workspace\test_code
