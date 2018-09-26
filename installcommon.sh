ae87a6aed96db682ca149c794ce54ac7c25b6846
ysoftman <ysoftman@gmail.com>
2018-09-26 19:05:44 +0900

    pyenv 공통 설치 요소에서 제외

--- installcommon.sh
+++ installcommon.sh
@@ -5,12 +5,10 @@ if [ $(uname) == 'Darwin' ]; then
 	echo 'OSX Environment'
 	# brew 설치
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	# zsh , pyenv 설치
-	brew install zsh pyenv
+	# zsh 설치
+	brew install zsh
 	# pip 설치
 	sudo easy_install pip
-	pyenv install -f 2.7.14
-	pyenv install -f 3.6.4
 elif [ $(uname) == 'Linux' ]; then
 	echo 'Linux Environment'
 	# yum 실행보기
