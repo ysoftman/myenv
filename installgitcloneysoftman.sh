873f39284a8885a74bf53f3842eaaaf10824420f
ysoftman <ysoftman@gmail.com>
2018-09-27 09:07:14 +0900

    git clone and pull

--- installgitcloneysoftman.sh
+++ installgitcloneysoftman.sh
@@ -6,11 +6,19 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
-git clone https://github.com/ysoftman/aleng.git
-git clone https://github.com/ysoftman/codingtest.git
-git clone https://github.com/ysoftman/cutstring.git
-git clone https://github.com/ysoftman/enchash.git
-git clone https://github.com/ysoftman/myenv.git
-git clone https://github.com/ysoftman/taja.git
-git clone https://github.com/ysoftman/test_code.git
-git clone https://github.com/ysoftman/ysoftman.github.io.git
+git_clone_and_pull()
+{
+    git clone ${1}
+    targetdir=`echo ${1} | sed 's/^.*ysoftman\///' | sed 's/\.git$//'`
+    git -C ~/workspace/${targetdir} pull
+}
+
+
+git_clone_and_pull https://github.com/ysoftman/aleng.git
+git_clone_and_pull https://github.com/ysoftman/codingtest.git
+git_clone_and_pull https://github.com/ysoftman/cutstring.git
+git_clone_and_pull https://github.com/ysoftman/enchash.git
+git_clone_and_pull https://github.com/ysoftman/myenv.git
+git_clone_and_pull https://github.com/ysoftman/taja.git
+git_clone_and_pull https://github.com/ysoftman/test_code.git
+git_clone_and_pull https://github.com/ysoftman/ysoftman.github.io.git
