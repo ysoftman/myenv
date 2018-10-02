040ffcbb78e918f73ee8f59f6a4b74ae7e485be8
ysoftman <ysoftman@gmail.com>
2018-10-02 15:30:42 +0900

    설정 백업

--- backupmysetting.sh
+++ backupmysetting.sh
@@ -17,27 +17,20 @@ pip list | sed -n '3,$p' | awk '{print $1}' | tr '\n' ' ' >> ${install_file}
 # --upgrade 필요시에만 사용
 # echo ' --upgrade' >> ${install_file}
 
-# backup my shell settings
-cp -v ~/.bashrc ./.bashrc.backup
-cp -v ~/.zshrc ./.zshrc.backup
-
-# backup vimrc
-cp -v ~/.vimrc ./.vimrc.backup
-
 # backup .ssh directory
-# 보안사항으로 실제 필요할때만 사용하자
+# 보안사항으로 github 저장소에 올리면 안됨.
 # mkdir -p .ssh
 # cp -v ~/.ssh/* ./.ssh/
 
 # backup hosts
-# 보안사항으로 실제 필요할때만 사용하자
-#cp -v /etc/hosts ./hosts
+# 보안사항으로 github 저장소에 올리면 안됨.
+# cp -v /etc/hosts ./hosts
 
 # backup vscode settings
 mkdir -p vscode_settings
 cp -v ~/Library/Application\ Support/Code/User/*.json ./vscode_settings/
 
 # backup vscode extension and make install script
-install_file="install_vscode_extension.sh"
+install_file="installvscodeextension.sh"
 echo '#!/bin/sh' > ${install_file}
-code --list-extensions  | sed 's/^/code --install-extension /g' >> ${install_file}
+code --list-extensions | sed 's/^/code --install-extension /g' >> ${install_file}
