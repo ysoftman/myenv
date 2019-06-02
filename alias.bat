21c79f8d6ad3c374efdffbbb50206d2fe60f96af
ysoftman <ysoftman@gmail.com>
2019-06-02 16:55:14 +0900

    alias.bat 자동 실행 삭제

--- alias.bat
+++ alias.bat
@@ -1,9 +1,13 @@
-cls
+@cls
 @REM add registry cmd autorun
-reg add "HKCU\Software\Microsoft\Command Processor" /v autorun /t reg_multi_sz /d d:\ysoftman\workspace\myenv\alias.bat /f
-reg query "HKCU\Software\Microsoft\Command Processor" /v autorun
+@rem reg add "HKCU\Software\Microsoft\Command Processor" /v autorun /t reg_multi_sz /d c:\ysoftman\workspace\myenv\alias.bat /f
+@rem reg query "HKCU\Software\Microsoft\Command Processor" /v autorun
+
+@rem wsl 환경에서 vscode 실행 에러로 alais.bat 자동실행 삭제
+@rem delete registry cmd autorun
+@rem reg delete "HKCU\Software\Microsoft\Command Processor"
 
 @REM set alias(doskey)
 doskey vi=vim
-doskey work=cd /d d:\ysoftman\workspace
-doskey testcode=cd /d d:\ysoftman\workspace\test_code
+doskey work=cd /d c:\ysoftman\workspace
+doskey testcode=cd /d c:\ysoftman\workspace\test_code
