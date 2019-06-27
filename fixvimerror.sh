9c4f3ad9fce3fac15e336399b60a5da8c7476613
ysoftman <ysoftman@gmail.com>
2019-06-27 19:25:34 +0900

    add fixvimerror.sh

---
+++ fixvimerror.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# The ycmd server SHUT DOWN (restart with :YcmRestartServer)" 메시지가 발생하는 경우
+cd ${HOME}/.vim/plugged/youcompleteme/
+git submodule update --init --recursive
+python ./install.py
