d488eeb9e1057eb4aaa8ebbd0b90cb055a29c329
ysoftman <ysoftman@gmail.com>
2019-06-27 19:20:30 +0900

    add android env

--- myenv.sh
+++ myenv.sh
@@ -11,6 +11,8 @@ export EDITOR=vim
 export VISUAL=vim
 export ANSIBLE_NOCOWS=1 # disable cowsay message when using ansible
 
+unalias ls
+
 if [[ $(uname) == 'Darwin' ]]; then
     export LSCOLORS='GxFxCxDxBxegedabagaced'
     export CLICOLOR=1
@@ -21,7 +23,7 @@ elif [[ $(uname) == 'Linux' ]]; then
     export LC_ALL=ko_KR.utf8
     #export PS1="\u@\h:\w\$ "
     export LS_COLORS='no=00:fi=00:di=00;36:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:ow=01;36;40:*.sh=00;32'
-    alias ll='ls -ahl --color=auto'
+    alias ll='ls -ahl'
     # WSL(Windows Subsystem for Linux)
     if [[ $(uname -r | sed 's/-/ /g' | awk '{print $3}') == 'Microsoft' ]]; then
         alias netstat='/mnt/c/Windows/System32/netstat.exe'
