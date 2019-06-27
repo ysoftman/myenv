cb2509458529f70fa3c7fe95f89fd653658b9a87
ysoftman <ysoftman@gmail.com>
2019-06-27 20:49:20 +0900

    update android settings.

--- fixvimerror.sh
+++ fixvimerror.sh
@@ -2,4 +2,4 @@
 # The ycmd server SHUT DOWN (restart with :YcmRestartServer)" 메시지가 발생하는 경우
 cd ${HOME}/.vim/plugged/youcompleteme/
 git submodule update --init --recursive
-python ./install.py
+python2 ./install.py
