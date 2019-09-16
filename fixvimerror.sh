f6840a32009257f319596e840b060d3a1ed5dac1
ysoftman <ysoftman@gmail.com>
2019-09-16 10:47:18 +0900

    install vim plugin

--- fixvimerror.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# The ycmd server SHUT DOWN (restart with :YcmRestartServer)" 메시지가 발생하는 경우
-cd ${HOME}/.vim/plugged/youcompleteme/
-git submodule update --init --recursive
-python2 ./install.py
