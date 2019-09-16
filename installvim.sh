b9505ee33aa83add7876536587f6467bf6151de3
ysoftman <ysoftman@gmail.com>
2019-09-16 10:47:18 +0900

    install vim plugin

--- installvim.sh
+++ installvim.sh
@@ -94,3 +94,19 @@ mkdir -p ~/.vim/bundle
 
 # 패키지 관리를 위한 Plug 설치
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# .vimrc 링크
+[ -h ~/.vimrc ] && unlink ~/.vimrc
+[ -f ~/.vimrc ] && mv -fv ~/.vimrc ~/.vimrc.bak
+ln -s ${PWD}/.vimrc ~/.vimrc
+
+# 터미널에서 플러그인 설치후 vim 종료
+echo 'yes' | vim +PlugInstall +qall 2> /dev/null
+
+# vim 실행 후 GoInstallBinaries 로 $GOPATH/bin 에 필요한 파일들이 설치하고 모두 종료
+# dockerfile 로 이미지 빌드시 사용자 입력을 받을 수 없으니 slient 모드로 설치
+vim +'silent :GoInstallBinaries' +qall
+
+# The ycmd server SHUT DOWN (restart with :YcmRestartServer)" 메시지가 발생하는 경우
+cd ~/.vim/plugged/youcompleteme/ && git submodule update --init --recursive
+./install.py
