7604b3e909185feba66c9db33c7d8634bfb007da
ysoftman <ysoftman@gmail.com>
2020-07-30 18:08:31 +0900

    update install vim script

--- installvim.sh
+++ installvim.sh
@@ -2,13 +2,31 @@
 # ysoftman
 # vim 설치 스크립트
 
-reinstallarg=""
-if (($# >= 1)); then
-	if [[ $1 == 'reinstall' ]]; then
-		reinstallarg=$1
-		echo $reinstallarg
+install_vim_from_src() {
+	# 최신 버전 vim 설치
+	if [ ! -d 'vim-8.0.1432' ]; then
+		# git clone https://github.com/vim/vim.git
+		wget "https://github.com/vim/vim/archive/v8.0.1432.tar.gz"
+		tar zxvf v8.0.1432.tar.gz
+	fi
+	cd vim-8.0.1432/src
+	make distclean
+	# youcompleteme 플러그인이 python 을 사용하기때문에  python2,3 을 지원하는 vim 으로 빌드되어야 한다.
+	# ruby, lua 지원도 포함해두자
+	./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-rubyinterp=yes --enable-luainterp=yes
+	make -j 8
+	sudo make install
+}
+
+
+if (( $# >= 1 )); then
+	# 강제 재설치하는 경우
+	if [[ $1 == 'install_from_src' ]]; then
+		echo "install_vim_from_src()..."
+		install_vim_from_src
+		exit 0
 	else
-		echo '[if you want to reinstall vim] bash' $0 reinstall
+		echo '[if you want to install_from_src vim] bash' $0 install_from_src
 		exit 1
 	fi
 fi
@@ -43,22 +61,6 @@ else
 	exit
 fi
 
-installvim() {
-	# 최신 버전 vim 설치
-	if [ ! -d 'vim-8.0.1432' ]; then
-		# git clone https://github.com/vim/vim.git
-		wget "https://github.com/vim/vim/archive/v8.0.1432.tar.gz"
-		tar zxvf v8.0.1432.tar.gz
-	fi
-	cd vim-8.0.1432/src
-	make distclean
-	# youcompleteme 플러그인이 python 을 사용하기때문에  python2,3 을 지원하는 vim 으로 빌드되어야 한다.
-	# ruby, lua 지원도 포함해두자
-	./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-rubyinterp=yes --enable-luainterp=yes
-	make -j 8
-	sudo make install
-}
-
 # 설치된 vim 버전이 너무 낮으면 소스 받아서 설치
 base_vim_version="8.0"
 cur_vim_version=$(vim --version | grep 'Vi IMproved' | awk '{print $5}')
@@ -66,18 +68,11 @@ echo "cur_vim_version : ${cur_vim_version}"
 highest_version="$(printf "${cur_vim_version}\n${base_vim_version}" | sort -r | head -n1)"
 if [[ ${highest_version} != ${cur_vim_version} ]]; then
 	echo "cur_vim_version < ${base_vim_version}"
-	echo "installvim()..."
-	installvim
+	echo "install_vim_from_src()..."
+	install_vim_from_src
 else
 	echo "cur_vim_version >= ${base_vim_version}"
 fi
-
-# 강제 재설치하는 경우
-if [[ ${reinstallarg} == "reinstall" ]]; then
-	echo "installvim()..."
-	installvim
-fi
-
 
 GOPATH=${HOME}/workspace/gopath
 echo GOPATH=${GOPATH}
