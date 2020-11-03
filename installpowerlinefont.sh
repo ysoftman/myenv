fb024fbe4ad86246c07c181583689c0934d2b3db
ysoftman <ysoftman@gmail.com>
2020-11-03 20:44:38 +0900

    install termux font

--- installpowerlinefont.sh
+++ installpowerlinefont.sh
@@ -4,6 +4,9 @@ git clone https://github.com/powerline/fonts.git
 cd fonts
 bash install.sh
 cd ..
+if [ -d ~/.termux ]; then
+	cp -v fonts/Hack/Hack-Regular.ttf ~/.termux/font.ttf
+fi
 rm -rfv fonts
 
 # iterm2 -> preferences -> profiles -> text -> non-ascii font 에서 powerline 용 폰트 중 하나 선택
