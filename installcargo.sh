549026b2d7393f1a0842cea48fba25b050bf29c5
ysoftman <ysoftman@gmail.com>
2022-01-03 01:26:31 +0900

    add cargo install

---
+++ installcargo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# https://github.com/ogham/exa
+# https://github.com/Peltoche/lsd
+# https://github.com/sharkdp/fd
+# https://github.com/bootandy/dust
+# https://github.com/imsnif/diskonaut
+# https://github.com/chmln/sd
+# https://github.com/BurntSushi/ripgrep
+# https://github.com/sharkdp/bat
+# https://github.com/dalance/procs
+# https://github.com/ClementTsang/bottom
+# https://github.com/imsnif/bandwhich
+# https://github.com/sharkdp/hexyl
+# https://github.com/sharkdp/hyperfine
+cargo install -j 4 exa lsd fd-find du-dust diskonaut sd ripgrep bat procs btm bandwhich hexyl hyperfine
+# 설치 확인
+cargo install --list
