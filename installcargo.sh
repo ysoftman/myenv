9c2bf990bb632f2a424d76e681e196b8914934fe
ysoftman <ysoftman@gmail.com>
2022-01-03 12:44:22 +0900

    add rust update

--- installcargo.sh
+++ installcargo.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# rust 버전업
+rustup update
+
 # https://github.com/ogham/exa
 # https://github.com/Peltoche/lsd
 # https://github.com/sharkdp/fd
@@ -13,5 +17,6 @@
 # https://github.com/sharkdp/hexyl
 # https://github.com/sharkdp/hyperfine
 cargo install -j 4 exa lsd fd-find du-dust diskonaut sd ripgrep bat procs btm bandwhich hexyl hyperfine
+
 # 설치 확인
 cargo install --list
