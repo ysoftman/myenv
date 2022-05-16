c1da97b24a418b92c8888aaec2ed41fae77de616
ysoftman <ysoftman@gmail.com>
2022-05-16 10:45:21 +0900

    prevent to fail to add yaml, yml file to KUBECONFIG

--- myenv.sh
+++ myenv.sh
@@ -35,7 +35,8 @@ fi
 # set kubeconfig path
 if [ -d ${HOME}/.kube ]; then
     export KUBECONFIG="${HOME}/.kube/config"
-    for i in $(ls ${HOME}/.kube/*.{yaml,yml}); do { KUBECONFIG+=":"$i; } done
+    for i in $(ls ${HOME}/.kube/*.yaml); do { KUBECONFIG+=":"$i; } done 2> /dev/null
+    for i in $(ls ${HOME}/.kube/*.yml); do { KUBECONFIG+=":"$i; } done 2> /dev/null
     # KUBECONFIG 파일들 하나로 합칠때
     # kubectl config view --flatten > ${HOME}/.kube/z
 fi
