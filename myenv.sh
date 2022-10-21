d8d2c118829905ab4a9a8b96511bf8a114c0b042
ysoftman <ysoftman@gmail.com>
2022-10-21 19:11:26 +0900

    make get_git_url function

--- myenv.sh
+++ myenv.sh
@@ -242,6 +242,7 @@ rm -rfv ${golang_version}.tar.gz"
 
 # load my functions
 source ${HOME}/workspace/myenv/rename_files.sh
+source ${HOME}/workspace/myenv/get_git_url.sh
 source ${HOME}/workspace/myenv/k8s_info.sh
 
 temp=$(which pyenv 2> /dev/null)
