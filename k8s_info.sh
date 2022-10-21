da21f42272151cb25f5f23bbd9b97d17f8cd87fa
ysoftman <ysoftman@gmail.com>
2022-10-21 23:15:20 +0900

    share common colors.sh

--- k8s_info.sh
+++ k8s_info.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-reset_color='\033[0m'
-black='\033[0;30m'
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-blue='\033[0;34m'
-purple='\033[0;35m'
-cyan='\033[0;36m'
-white='\033[0;37m'
-darkgray='\033[1;30m'
-lightred='\033[1;31m'
-lightgreen='\033[1;32m'
-lightyellow='\033[1;33m'
-lightblue='\033[1;34m'
-lightpurple='\033[1;35m'
-lightcyan='\033[1;36m'
-lightwhite='\033[1;37m'
-
+source ${HOME}/workspace/myenv/colors.sh
 
 function cnt_pods_in_nodes() {
     ret=$(kubectl)
--- myenv.sh
+++ myenv.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-reset_color="\033[0m"
-black="\033[0;30m"
-red="\033[0;31m"
-green="\033[0;32m"
-yellow="\033[0;33m"
-blue="\033[0;34m"
-purple="\033[0;35m"
-cyan="\033[0;36m"
-white="\033[0;37m"
+source ${HOME}/workspace/myenv/colors.sh
 
 current_shell="bash"
 if [[ $(ps -p $$ -o command | sed -e 1d) == *"bash"* ]]; then
