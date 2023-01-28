4c7be0fc9902d929c8cde374d7df3fe45d4a69e9
ysoftman <ysoftman@gmail.com>
2023-01-28 14:20:01 +0900

    update cntsrc function

--- cnt_src.sh
+++ cnt_src.sh
@@ -10,38 +10,35 @@ cntsrc() {
     medium_files=()
     hard_files=()
     no_level_files=()
+    echo -n "processing"
     for f in $(fd ".go|.cpp|.c|.sh|.sql" --exclude="ysoftman_*"); do
-        r=$(rg -i -N "Easy|Medium|Hard" --color=never --no-filename --max-count 1 ${f} | sd "^(# )"  "")
-        if [[ $r == "Easy" ]]; then
+        header=$(rg -i -N "Easy|Medium|Hard" --color=never --no-filename --max-count 1 -B1 ${f} | sd "^(# )"  "")
+        title=$(echo $header | head -1)
+        level=$(echo $header | tail -1)
+        if [[ $level == "Easy" ]]; then
             cnt_easy=$((cnt_easy+1))
-            easy_files+=($f)
-        elif [[ $r == "Medium" ]]; then
+            easy_files+=("${title} -> ${f}\n")
+        elif [[ $level == "Medium" ]]; then
             cnt_medium=$((cnt_medium+1))
-            medium_files+=($f)
-        elif [[ $r == "Hard" ]]; then
+            medium_files+=("${title} -> ${f}\n")
+        elif [[ $level == "Hard" ]]; then
             cnt_hard=$((cnt_hard+1))
-            hard_files+=($f)
+            hard_files+=("${title} -> ${f}\n")
         else
             cnt_no_level=$((cnt_no_level+1))
-            no_level_files+=($f)
+            no_level_files+=("${title} -> ${f}\n")
         fi
+        echo -n "."
     done
-    echo "[Easy Files]"
-    for f in ${easy_files[@]}; do
-        echo ${f}
-    done
-    echo "[Medium Files]"
-    for f in ${medium_files[@]}; do
-        echo ${f}
-    done
-    echo "[Hard Files]"
-    for f in ${hard_files[@]}; do
-        echo ${f}
-    done
-    echo "[No Level Files]"
-    for f in ${no_level_files[@]}; do
-        echo ${f}
-    done
+    echo "\n[Easy Files]"
+    echo ${easy_files[@]} | sort -h | sd "^ " "" | sed 1d
+    echo "\n[Medium Files]"
+    echo ${medium_files[@]} | sort -h | sd "^ " "" | sed 1d
+    echo "\n[Hard Files]"
+    echo ${hard_files[@]} | sort -h | sd "^ " "" | sed 1d
+    echo "\n[No Level Files]"
+    echo ${no_level_files[@]} | sort -h | sd "^ " "" | sed 1d
+
     echo "${cyan}Easy:" ${cnt_easy}${reset_color}
     echo "${yellow}Medium:" ${cnt_medium}${reset_color}
     echo "${red}Hard:" ${cnt_hard}${reset_color}
