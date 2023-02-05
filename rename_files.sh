220f6f2120269c670f4069f8925367f8b7bf35af
ysoftman <ysoftman@gmail.com>
2023-02-06 00:58:12 +0900

    fix 'File name too long' error

--- rename_files.sh
+++ rename_files.sh
@@ -2,10 +2,10 @@
 help_rename_files() {
     cat << zzz
 rename (*.go) snakecase to kebabcase
-ex) $0 .go kebab
+ex) rename_files .go kebab
 
 rename (*.go) kebabcase to snakecase
-ex) $0 .go snake
+ex) rename_files .go snake
 zzz
 }
 
@@ -22,7 +22,10 @@ rename_files() {
     fi
     # echo $1
     # echo $2
-
+    # IFS(Internal Field Separator) 를 space(디폴트)면
+    # fd 결과 파일들이 한줄로 한번에 처리되는데 이때 File name too long 에러가 발생한다.
+    # IFS 를 newline 로해 파일 1개씩 처리되도록 해야 한다.
+    IFS=$'\n'
     for from in $(fd $1$); do
         if [[ $2 == 'snake' ]]; then
             to=$(echo $from | sed s/-/_/g)
@@ -33,4 +36,5 @@ rename_files() {
             mv -v $from $to;
         fi
     done
+    IFS=' '
 }
