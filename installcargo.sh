2084d248465f45ab3847a656ac1dd7b2c281d29c
ysoftman <ysoftman@gmail.com>
2023-02-08 09:42:53 +0900

    install git-delta

--- installcargo.sh
+++ installcargo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rustup update
-cargo install bandwhich bat btm diskonaut dust exa fd hello_cargo hexyl hyperfine lsd ohmystock procs rg rust_grep sd zellij 
+cargo install bandwhich bat btm diskonaut dust exa fd hello_cargo hexyl hyperfine lsd ohmystock procs rg rust_grep sd zellij git-delta
