55f669462fd7b19d22e28fa362a6798fdad9eca6
ysoftman <ysoftman@gmail.com>
2023-07-16 00:59:18 +0900

    update rg to ripgrep

--- installcargo.sh
+++ installcargo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo install bandwhich bat btm diskonaut dust exa fd hello_cargo hexyl hyperfine lsd ohmystock procs rg rust_grep sd zellij
+cargo install bandwhich bat btm diskonaut dust exa fd hello_cargo hexyl hyperfine lsd ohmystock procs ripgrep rust_grep sd zellij
