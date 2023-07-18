30bfa298e38e5c217c2057eeb95b8e5820f122f2
ysoftman <ysoftman@gmail.com>
2023-07-18 20:08:59 +0900

    update fd-find name

--- installcargo.sh
+++ installcargo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo install bandwhich bat btm diskonaut dust exa fd hello_cargo hexyl hyperfine lsd ohmystock procs ripgrep rust_grep sd zellij git-delta
+cargo install bandwhich bat btm diskonaut dust exa fd-find hello_cargo hexyl hyperfine lsd ohmystock procs ripgrep rust_grep sd zellij git-delta
