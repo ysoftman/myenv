97b24fa98bec61ecfce113912cf2e2f387c0438b
ysoftman <ysoftman@gmail.com>
2023-08-14 18:02:52 +0900

    update

--- installcargo.sh
+++ installcargo.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-# rustup 을 설치해서 rustc/cargo 버전을 올려보자.
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# cargo 가 제대로 동작하지 않는다면 다음과 같이 삭제 후 재설치한다.
-#rustup uninstall stable && rustup install stable
-
-cargo install bandwhich bat bottom diskonaut du-dust exa fd-find hello_cargo hexyl hyperfine lsd procs ripgrep rust_grep sd zellij git-delta
+rustup update
+cargo install bandwhich bat btm diskonaut dust exa fd delta hello_cargo hexyl hyperfine lsd ohmystock procs rg rust_grep sd zellij 
