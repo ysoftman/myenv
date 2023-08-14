ae803ef61343b5994681bcbd2587c9040d1f7a7a
ysoftman <ysoftman@gmail.com>
2023-08-15 00:40:06 +0900

    check backup data

--- installcargo.sh
+++ installcargo.sh
@@ -6,4 +6,5 @@
 # cargo 가 제대로 동작하지 않는다면 다음과 같이 삭제 후 재설치한다.
 #rustup uninstall stable && rustup install stable
 
-cargo install bandwhich bat bottom diskonaut du-dust exa fd-find hello_cargo hexyl hyperfine lsd procs ripgrep rust_grep sd zellij git-delta
+rustup update
+cargo install bandwhich bat btm diskonaut dust exa fd delta hello_cargo hexyl hyperfine lsd procs rg rust_grep sd zellij
