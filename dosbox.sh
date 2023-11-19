ffc40e086559cd99e8aecf118d5787ce084aca1d
ysoftman <ysoftman@gmail.com>
2023-11-20 01:31:44 +0900

    update

--- dosbox.sh
+++ dosbox.sh
@@ -1,1 +1,6 @@
-open -a DOSBox --args -conf ${myenv_path}/dosbox.conf
+if which dosbox; then
+    dosbox --args -conf ${myenv_path}/dosbox.conf
+else
+    open -a DOSBox --args -conf ${myenv_path}/dosbox.conf
+fi
+
