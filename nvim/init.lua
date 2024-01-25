9c0ef91663fd191c71db4b99ca69e50b45529dad
ysoftman <ysoftman@gmail.com>
2024-01-25 12:01:01 +0900

    add telescope keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,10 @@
 local vimrc = vim.fn.stdpath("config") .. "/_init.vim"
 vim.cmd.source(vimrc)
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 
