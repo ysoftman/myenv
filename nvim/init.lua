bb08a071d2d4e2b0d8efeaec4426a1f28b89415e
ysoftman <ysoftman@gmail.com>
2024-01-26 10:45:44 +0900

    add comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,10 @@
 local vimrc = vim.fn.stdpath("config") .. "/_init.vim"
 vim.cmd.source(vimrc)
 
+-- neovim 에서 jedi-vim 사용을 위해 pynvim 설치 필요
+-- pip install pynvim
+
+-- telescope keymap
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
