a6cbc5f60b696179ad2a961f801a8bbc433a44eb
ysoftman <ysoftman@gmail.com>
2024-01-26 15:25:12 +0900

    block neovim lua plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,23 @@
 local vimrc = vim.fn.stdpath("config") .. "/_init.vim"
 vim.cmd.source(vimrc)
 
--- neovim 에서 jedi-vim 사용을 위해 pynvim 설치 필요
--- pip install pynvim
+--lazy plugin
+--local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--if not vim.loop.fs_stat(lazypath) then
+--  vim.fn.system({
+--    "git",
+--    "clone",
+--    "--filter=blob:none",
+--    "https://github.com/folke/lazy.nvim.git",
+--    "--branch=stable", -- latest stable release
+--    lazypath,
+--  })
+--end
+--vim.opt.rtp:prepend(lazypath)
+
+--vim plug 와 동시에 사용되지 않아 주석처리함
+--require("lazy").setup("plugins")
+
 
 -- telescope keymap
 local builtin = require('telescope.builtin')
