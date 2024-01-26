c55155cf0ebfe6fcc54a0982d025e9a269772312
ysoftman <ysoftman@gmail.com>
2024-01-26 10:32:00 +0900

    remove lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,13 +2,3 @@
 local vimrc = vim.fn.stdpath("config") .. "/_init.vim"
 vim.cmd.source(vimrc)
 
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("plugins.example")
-
-
---local builtin = require("telescope.builtin")
---vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
---vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
---vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
---vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
