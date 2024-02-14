6483059c89ae59e91b7a0c25c0c3c76e66b98c4d
ysoftman <ysoftman@gmail.com>
2024-02-15 01:55:12 +0900

    update

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+--if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
