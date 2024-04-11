1ae3dc839bb5cbc98ff8f1cb4b1214e937148c81
ysoftman <ysoftman@gmail.com>
2024-04-11 22:43:19 +0900

    update

--- installlapcesettings.sh
+++ installlapcesettings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $(uname) == 'Darwin' ]; then
-    ln -sf ./lapce_settings/keymaps.toml ~/Library/Application\ Support/dev.lapce.Lapce-Stable/keymaps.toml
-    ln -sf ./lapce_settings/settings.toml ~/Library/Application\ Support/dev.lapce.Lapce-Stable/settings.toml
+    ln -sf ${PWD}/lapce_settings/keymaps.toml ~/Library/Application\ Support/dev.lapce.Lapce-Stable/keymaps.toml
+    ln -sf ${PWD}/lapce_settings/settings.toml ~/Library/Application\ Support/dev.lapce.Lapce-Stable/settings.toml
 else
     echo 'unknown system'
     exit 1
