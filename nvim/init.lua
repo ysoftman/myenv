ab23ece2489cc1beb863bfda7d7ebfdea8c537a1
ysoftman <ysoftman@gmail.com>
2024-05-16 19:19:55 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,11 +5,14 @@
 local vimrc = vim.fn.stdpath("config") .. "/_init.vim"
 vim.cmd.source(vimrc)
 
-
 -- telescope keymap
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<a-t>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<c-f>", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<c-l>", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fr", builtin.registers, {})
+vim.keymap.set("n", "<a-r>", builtin.registers, {})
