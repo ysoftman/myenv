6c2e61e96166718d0d9621b3ea729c0db1e74897
ysoftman <ysoftman@gmail.com>
2024-06-02 15:24:06 +0900

    update

--- installpip.sh
+++ installpip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo pip install --upgrade pip
-sudo pip install ansible ansible-core bottle certifi cffi charset-normalizer click cryptography idna Jinja2 jmespath MarkupSafe mysql-connector-python netaddr packaging pbr pip pycparser pytimeparse2 PyYAML requests resolvelib ruamel.yaml ruamel.yaml.clib simplejson sqlite3-to-mysql tabulate tqdm typing_extensions Unidecode urllib3 
+sudo pip install ansible ansible-core bottle certifi cffi charset-normalizer click cryptography idna Jinja2 jmespath MarkupSafe mysql-connector-python netaddr packaging pbr pip pycparser pytimeparse2 PyYAML requests resolvelib ruamel.yaml ruamel.yaml.clib simplejson sqlite3-to-mysql tabulate tqdm typing_extensions Unidecode unimatrix urllib3 
