52bf7ece185f6ddce24a4262cb39355477d7f9c9
ysoftman <ysoftman@gmail.com>
2024-06-30 13:26:57 +0900

    cargo packages are just managed by installcargo.sh

--- installcargo.sh
+++ installcargo.sh
@@ -58,7 +58,6 @@ done
 echo "install_pkgs=$install_pkgs"
 echo "uninstall_pkgs=$uninstall_pkgs"
 
-
 # rustup 을 설치해서 rustc/cargo 버전을 올려보자.
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -68,3 +67,7 @@ echo "uninstall_pkgs=$uninstall_pkgs"
 rustup update
 cargo install ${install_pkgs}
 cargo uninstall ${uninstall_pkgs} 2> /dev/null
+
+echo "-- show installed packages by cargo --"
+cargo install --list | awk 'NR%2==0 {print $1}'
+
