7f790ed538fffc0801cff93f5517501e361026b4
ysoftman <ysoftman@gmail.com>
2024-07-30 10:34:05 +0900

    update iter-mcolor source

--- installitermcolor.sh
+++ installitermcolor.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ ! -d "iterm-colors" ]; then
-    git clone https://github.com/bahlo/iterm-colors
+    git clone https://github.com/mbadolato/iTerm2-Color-Schemes
 else
     echo "iterms-colors already exist."
 fi
+
+iTerm2-Color-Schemes/tools/import-scheme.sh iTerm2-Color-Schemes/schemes/*
+rm -rf iTerm2-Color-Schemes
