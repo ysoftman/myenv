ef4fa0ed5a699eaa7e0bc8d2f45f6d436d001088
ysoftman <ysoftman@gmail.com>
2024-08-08 22:27:56 +0900

    update installgolang.sh installkubectl.sh

--- installgolang.sh
+++ installgolang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $# != 1 ]]; then
-    echo "ex) bash ${0} go1.19.2.darwin-amd64"
-    echo "ex) bash ${0} go1.19.2.linux-amd64"
+    echo "ex) bash ${0} go1.22.6.darwin-amd64"
+    echo "ex) bash ${0} go1.22.6.linux-amd64"
     exit 0
 fi
 golang_version=${1}
--- installkubectl.sh
+++ installkubectl.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
+printf "install kubectl/kubecolor in %s\n" $(uname)
+
 if [[ $(uname) == 'Darwin' ]]; then
     brew install kubectl kubecolor
 elif [[ $(uname) == 'Linux' ]]; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
     echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     kubectl version --client
+    rm -f kubectl kubectl.sha256
+    # Requires Go 1.22 (or later)
+    # bash ./installgolang.sh go1.22.6.linux-amd64
     go install github.com/kubecolor/kubecolor@latest
 fi
