d5fe1c0df284fa545cb42d6c87ca1ccb9c2617ae
ysoftman <ysoftman@gmail.com>
2024-12-12 01:36:08 +0900

    add rg arguments

--- nvim/lua/plugins/ysoftman_plugins.lua
+++ nvim/lua/plugins/ysoftman_plugins.lua
@@ -81,12 +81,13 @@ return {
                 winblend = 0,
                 vimgrep_arguments = {
                     'rg',
-                    '--color=never',
                     '--no-heading',
                     '--with-filename',
                     '--line-number',
                     '--column',
-                    '--smart-case',
+                    "--fixed-strings", -- Treat all patterns as literals instead of as regular expressions. When this flag is used, special regular expression meta characters such as .(){}*+ should not need be escaped.
+                    "--hidden",
+                    "--smart-case", -- 소문자로만 입력시 대소문자 구분하지 않음, 아니면 대소문자 구분
                     -- Add your custom rg options here
                 },
                 mappings = {
