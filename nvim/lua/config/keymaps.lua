495a6e61ea5404cdbf46b46359f70ac53cef684d
ysoftman <ysoftman@gmail.com>
2024-12-13 14:17:25 +0900

    comment out custom comment toggle keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -46,6 +46,7 @@ lsp_keys[#lsp_keys + 1] = { "<leader>d", vim.lsp.buf.definition, desc = "Go to D
 lsp_keys[#lsp_keys + 1] = { "<leader>r", vim.lsp.buf.references, desc = "Go to References" }
 
 -- comment
-vim.keymap.set({ "n", "v" }, "<leader>cc", function()
-  return require("vim._comment").operator()
-end, { expr = true, desc = "Toggle comment" })
+-- codelens 에서 해당 단축키를 사용하고 있다. 그냥 default gcc, gc 등을 사용하자.
+-- vim.keymap.set({ "n", "v" }, "<leader>cc", function()
+--   return require("vim._comment").operator()
+-- end, { expr = true, desc = "Toggle comment" })
