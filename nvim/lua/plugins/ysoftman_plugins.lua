f3519a94d2c12c6036a431cad0f4c87bdb5123b8
ysoftman <ysoftman@gmail.com>
2024-12-17 10:41:12 +0900

    disable s-h, s-l keymap and update bufferline indicator

--- nvim/lua/plugins/ysoftman_plugins.lua
+++ nvim/lua/plugins/ysoftman_plugins.lua
@@ -34,9 +34,22 @@ return {
   },
 
   {
-    "LazyVim/LazyVim",
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
     opts = {
-      colorscheme = "catppuccin",
+      options = {
+        indicator = {
+          -- icon = "▎", -- style =  "icon"일때
+          icon = "✓",
+          -- style = "underline",
+          style = "icon",
+        },
+      },
+    },
+    keys = {
+      { "<S-h>", false },
+      { "<S-l>", false },
     },
   },
 
