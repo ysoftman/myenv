8b5fd611d06288cd1a0afe018b1c86f893081f7c
ysoftman <ysoftman@gmail.com>
2024-12-22 18:22:45 +0900

    update

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -68,6 +68,9 @@ return {
           LazyVim.lsp.on_attach(function(client, _)
             if not client.server_capabilities.semanticTokensProvider then
               local semantic = client.config.capabilities.textDocument.semanticTokens
+              if semantic == nil then
+                return
+              end
               client.server_capabilities.semanticTokensProvider = {
                 full = true,
                 legend = {
