e19d08cbc79b77c6ff49a42cd71b363eb4f43de1
ysoftman <ysoftman@gmail.com>
2024-12-26 19:48:02 +0900

    use deafult configuration

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -7,10 +7,6 @@ return {
         width = "block",
         right_pad = 1,
       },
-      heading = {
-        sign = false,
-        icons = {},
-      },
     },
     ft = { "markdown", "norg", "rmd", "org" },
   },
