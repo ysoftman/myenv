9f1b0e8715831a52b74384ae3df28d804f0debc1
ysoftman <ysoftman@gmail.com>
2025-01-09 22:30:43 +0900

    tidy plugins

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,4 +1,5 @@
 -- Mason은 Neovim에서 외부 도구(예: 언어 서버, 포매터, 린터)를 설치하고 관리하는 패키지 관리자입니다.
+-- :Mason 으로 설치 도구들 확인
 return {
   {
     "williamboman/mason.nvim",
@@ -19,6 +20,8 @@ return {
   {
     "williamboman/mason.nvim",
     opts = {
+      -- a list of all tools you want to ensure are installed upon
+      -- start
       ensure_installed = {
         "stylua",
         "shellcheck",
@@ -28,12 +31,11 @@ return {
         "marksman",
         "markdownlint",
       },
-      formatters_by_ft = {
-        ["html"] = { "prettier" },
-        ["md"] = { "prettier", "markdownlint" },
-        ["lua"] = { "stylua" },
-        ["py"] = { "black" },
-      },
+      -- automatically install / update on startup. If set to false nothing
+      -- will happen on startup. You can use :MasonToolsInstall or
+      -- :MasonToolsUpdate to install tools and check for updates.
+      -- Default: true
+      run_on_start = true,
     },
   },
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,10 +3,15 @@ return {
     "nvimtools/none-ls.nvim", -- for formatting, diagnostics, code-actions, completion, hover
     opts = function(_, opts)
       local nls = require("null-ls")
+      opts.root_dir = opts.root_dir
+        or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
       opts.sources = vim.list_extend(opts.sources or {}, {
         -- Add or modify sources here
+        nls.builtins.formatting.fish_indent,
+        nls.builtins.diagnostics.fish,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.shfmt,
         nls.builtins.formatting.prettier,
-        nls.builtins.diagnostics.eslint,
         nls.builtins.formatting.shfmt.with({
           -- confirm shfmt 보다 none-ls shfmt 설정이 적용된다.
           -- use 4spaces instead of tabs(default)
