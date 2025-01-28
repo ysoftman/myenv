a9a6c9e5661b196cedc0e1d86eb81fab152c2ad9
ysoftman <ysoftman@gmail.com>
2025-01-29 00:32:56 +0900

    update comments

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -13,7 +13,7 @@ return {
         nls.builtins.formatting.shfmt,
         nls.builtins.formatting.prettier,
         nls.builtins.formatting.shfmt.with({
-          -- confirm shfmt 보다 none-ls shfmt 설정이 적용된다.
+          -- conform shfmt 보다 none-ls shfmt 설정이 적용된다.
           -- use 4spaces instead of tabs(default)
           -- https://github.com/mvdan/sh/blob/master/cmd/shfmt/shfmt.1.scd#printer-flags
           extra_args = { "-i", "4", "-ci" },
