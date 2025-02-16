a27620d784c72bdf800dec8575b797f00f4fbe50
ysoftman <ysoftman@gmail.com>
2025-02-16 21:06:04 +0900

    separate installzsh from installcommon

--- installcommon.sh
+++ installcommon.sh
@@ -11,7 +11,7 @@ pkgs_pacman='zsh vim curl git tig tmux cmake ctags fortune-mod cowsay figlet cma
 pkgs_brew='zsh lsd exa rust git-delta ripgrep bat duf lolcat cowsay jq fastfetch neofetch'
 sudo_cmd='sudo'
 
-if [[ $(uname -o 2> /dev/null) == 'Android' ]]; then
+if [[ $(uname -o 2>/dev/null) == 'Android' ]]; then
     sudo_cmd=''
     package_program="pkg"
     ${sudo_cmd} ${package_program} update
@@ -32,13 +32,13 @@ elif [[ $(uname) == 'Darwin' ]]; then
 elif [[ $(uname) == 'Linux' ]]; then
     echo 'Linux Environment'
     # centos
-    if yum --version > /dev/null 2>&1; then
+    if yum --version >/dev/null 2>&1; then
         package_program="yum"
         ${sudo_cmd} ${package_program} update
         ${sudo_cmd} ${package_program} install -y ${pkgs_yum}
     else
         # ubuntu
-        if apt --version > /dev/null 2>&1; then
+        if apt --version >/dev/null 2>&1; then
             package_program="apt"
             ${sudo_cmd} ${package_program} update
             # 여러 패키지중 존재하지 않은 패키지가 포함되면 "E: Unable to locate package xxx" 에러가 발생하고 패키지가 하나도 설치 되지 않는다.
@@ -49,7 +49,7 @@ elif [[ $(uname) == 'Linux' ]]; then
             done
         fi
         # archlinux
-        if pacman --version > /dev/null 2>&1; then
+        if pacman --version >/dev/null 2>&1; then
             package_program="pacman"
             ${sudo_cmd} ${package_program} -Sy
             ${sudo_cmd} ${package_program} -S ${pkgs_pacman} --noconfirm
@@ -59,41 +59,7 @@ elif [[ $(uname) == 'Linux' ]]; then
     # export LC_ALL=ko_KR.utf8 사용을 위해서 정의되어 있어야 한다.
     ${sudo_cmd} localedef -f UTF-8 -i ko_KR ko_KR.utf8
 
-    # zsh 버전이 낮으면 소스 다운로드 받아 설치하기
-    cur_version="$(zsh --version | cut -d" " -f2)"
-    compare_version="5.1.999"
-    echo 'cur_version='${cur_version}
-    echo 'compare_version='${compare_version}
-    highest_version="$(printf "${cur_version}\n${compare_version}" | sort -r | head -n1)"
-    echo 'highest_version='${highest_version}
-    if [ "${highest_version}" == "${compare_version}" ]; then
-        echo "${compare_version} > ${cur_version}"
-        curl -OL https://sourceforge.net/projects/zsh/files/zsh/5.2/zsh-5.2.tar.gz/download
-        mv download zsh-5.2.tar.gz
-        tar zxvf zsh-5.2.tar.gz
-        cd zsh-5.2
-        ./configure && make -j 4 && ${sudo_cmd} make install
-        /usr/local/bin/zsh --version
-    else
-        echo "${compare_version} < ${cur_version}"
-    fi
 else
     echo 'This OS is not (Android or OS-X or Linux)'
     exit 1
-fi
-
-# 현재 유저의 기본 쉘을 zsh 로 변경
-# /etc/shells 는 >> 를 허용하지 않아 수정 파일로 바꿔친다.
-${sudo_cmd} cp -fv shells /etc/shells
-${sudo_cmd} cp -fv chsh /etc/pam.d/chsh
-if [ -x /opt/homebrew/bin/zsh ]; then
-    ${sudo_cmd} chsh -s /opt/homebrew/bin/zsh ${USER}
-elif  [ -x /usr/local/bin/zsh ]; then
-    ${sudo_cmd} chsh -s /usr/local/bin/zsh ${USER}
-elif [ -x /usr/bin/zsh ]; then
-    ${sudo_cmd} chsh -s /usr/bin/zsh ${USER}
-elif [ -x /bin/zsh ]; then
-    ${sudo_cmd} chsh -s /bin/zsh ${USER}
-else
-    echo 'can not find zsh'
 fi
