36503c18d460586a31e5bbd8c4d163e56a0f1b85
ysoftman <ysoftman@gmail.com>
2025-02-17 12:44:21 +0900

    separate neovim version

--- installneovim.sh
+++ installneovim.sh
@@ -10,13 +10,28 @@ if [[ $os_name == *"darwin"* ]]; then
     brew install neovim
     exit
 elif [[ $os_name == *"linux"* ]]; then
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+    base_version="2.29"
+    cur_version=$(ldd --version | head -1 | awk '{print $4}')
+    highest_version="$(echo -e "${cur_version}\n${base_version}" | sort -r | head -n1)"
+    if [[ ${highest_version} != "${cur_version}" ]]; then
+        echo "cur_version:$cur_version < ${base_version}"
+        echo "download neovim old verison"
+        filename="nvim-linux64"
+        url="https://github.com/neovim/neovim/releases/download/v0.9.2/$filename.tar.gz"
+    else
+        echo "cur_version:$cur_version >= ${base_version}"
+        echo "download neovim latest verison"
+        filename="nvim-linux-x86_64"
+        url="https://github.com/neovim/neovim/releases/latest/download/$filename.tar.gz"
+    fi
+
+    curl -LO $url
     sudo rm -rf /opt/nvim
-    sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+    sudo tar -C /opt -xzf $filename.tar.gz
 
     cat <<EOF
 Then add this to your shell config (~/.bashrc, ~/.zshrc, ...):
-export PATH="\$PATH:/opt/nvim-linux-x86_64/bin"
+export PATH="\$PATH:/opt/$filename/bin"
 EOF
 else
     echo "unknown os:$os_name"
