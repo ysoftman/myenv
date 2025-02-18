f0d49053c98f6da62bfaab3662231b2e9009b1a8
ysoftman <ysoftman@gmail.com>
2025-02-18 10:10:56 +0900

    add nvim bin path

--- myenv.sh
+++ myenv.sh
@@ -166,6 +166,10 @@ unset cat_cmd
 # mac 에선 yarn bin /opt/homebrew/bin 으로 설정되어 있어 tidy_path 실행전에 선언
 export PATH=$(yarn global bin 2>/dev/null):$PATH
 
+# linux 환경에서 수동 nvim 설치경로
+export PATH=/opt/nvim-linux64/bin:$PATH
+export PATH=/opt/nvim-linux-x86_64/bin:$PATH
+
 export GOPATH=$HOME/workspace/gopath
 tidy_path() {
     local temp_path=""
