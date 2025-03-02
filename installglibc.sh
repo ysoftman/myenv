a08cac9388613baea6fa906dbcc727a4bb559f25
ysoftman <ysoftman@gmail.com>
2025-03-02 23:22:53 +0900

    fix bug

--- installglibc.sh
+++ installglibc.sh
@@ -3,6 +3,7 @@
 echo "install glibc 2.29"
 wget https://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
 tar -zxvf glibc-2.29.tar.gz
+cd glibc-2.29
 mkdir build
 cd build
 ../configure --prefix=$HOME/glibc-2.29/build
