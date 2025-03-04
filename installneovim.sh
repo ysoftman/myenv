fec5fe141ec71ffb4edafcf71c3c1ec66d9aaf56
ysoftman <ysoftman@gmail.com>
2025-03-04 13:30:24 +0900

    add comments

--- installneovim.sh
+++ installneovim.sh
@@ -13,8 +13,20 @@ elif [[ $os_name == *"linux"* ]]; then
 
     cat <<EOF
 myenv.sh 에서 추가해놨습니다.
-Then add this to your shell config (~/.bashrc, ~/.zshrc, ...):
 export PATH="\$HOME/$filename/bin:\$PATH"
+
+#####
+
+# glibc 2.29 미만(ldd --version 확인) 환경에서는
+# glibc, gcc 설치
+bash installglibc.sh
+bash installgcc.sh
+cp -v ~/gcc-14.2.0/lib64/libgcc_s.so.1  ~/glibc-2.41/build/lib
+
+# nvim elf 동적라이브러리 경로 수정
+sudo dnf install patchelf
+patchelf --set-rpath ~/glibc-2.41/build/lib ~/nvim-linux-x86_64/bin/nvim
+patchelf --set-interpreter ~/glibc-2.41/build/lib/ld-linux-x86-64.so.2 ~/nvim-linux-x86_64/bin/nvim
 EOF
 
 else
