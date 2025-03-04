42decb3d0e32f2b21581e8a2765ff12c0517fdd5
ysoftman <ysoftman@gmail.com>
2025-03-04 11:32:06 +0900

    update

--- installglibc.sh
+++ installglibc.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 glibc_version="2.41"
+rm -rf glibc-$glibc_version
+rm -rf $HOME/glibc-$glibc_version
+
 echo "install glibc $glibc_version"
-wget https://ftp.gnu.org/gnu/glibc/glibc-$glibc_version.tar.gz
+if [ ! -f "glibc-$glibc_version.tar.gz" ]; then
+    wget https://ftp.gnu.org/gnu/glibc/glibc-$glibc_version.tar.gz
+fi
+
 tar -zxvf glibc-$glibc_version.tar.gz
 cd glibc-$glibc_version
 mkdir build
