e5996508556e227c7aef719813ee12a440146e2c
ysoftman <ysoftman@gmail.com>
2025-04-22 19:42:11 +0900

    use env vars in myenv.sh instead of custom zshrc.zsh-template

--- installohmyzsh.zsh
+++ installohmyzsh.zsh
@@ -4,6 +4,3 @@ rm -rfv ~/.oh-my-zsh
 # tools/install.sh 로 설치하면 .zshrc 를 새로 만들기 때문에 사용하지 않는다.
 git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
 
-# 미리 설정한 template 파일을 복사
-# .zshrc 에서 ~/.oh-my-zsh/templates/zshrc.zsh-template 로드하여 사용
-cp -v zshrc.zsh-template ~/.oh-my-zsh/templates/zshrc.zsh-template
