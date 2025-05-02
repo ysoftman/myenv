41daf952f3471b57516ae68126bb4f2c88e398a6
ysoftman <ysoftman@gmail.com>
2025-05-02 22:31:38 +0900

    add installstarship.sh

--- installohmyzsh.sh
+++ installohmyzsh.sh
@@ -1,6 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 
 rm -rfv ~/.oh-my-zsh
 # tools/install.sh 로 설치하면 .zshrc 를 새로 만들기 때문에 사용하지 않는다.
 git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
-
---
+++ installstarship.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+os_name=$(uname | tr '[:upper:]' '[:lower:]')
+if [[ $os_name == *"darwin"* ]]; then
+    brew install starship
+else
+    yes | curl -sS https://starship.rs/install.sh | sh
+fi
