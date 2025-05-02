c165947f06113f25aed56187c8cb36deb0ab7f5d
ysoftman <ysoftman@gmail.com>
2025-05-02 22:32:10 +0900

    update

--- installstarship.sh
+++ installstarship.sh
@@ -4,5 +4,5 @@ os_name=$(uname | tr '[:upper:]' '[:lower:]')
 if [[ $os_name == *"darwin"* ]]; then
     brew install starship
 else
-    yes | curl -sS https://starship.rs/install.sh | sh
+    curl -sS https://starship.rs/install.sh | sh
 fi
