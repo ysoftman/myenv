606657c610004533a105409db32b24de9a3e0505
ysoftman <ysoftman@gmail.com>
2025-05-09 11:58:06 +0900

    add linking config in each installation

--- installohmyposh.sh
+++ installohmyposh.sh
@@ -6,3 +6,9 @@ if [[ $os_name == *"darwin"* ]]; then
 else
     curl -s https://ohmyposh.dev/install.sh | bash -s
 fi
+
+export XDG_CONFIG_HOME="$HOME/.config"
+mkdir -p ${XDG_CONFIG_HOME}
+[ -h ${XDG_CONFIG_HOME}/ysoftman.omp.toml ] && unlink ${XDG_CONFIG_HOME}/ysoftman.omp.toml
+[ -f ${XDG_CONFIG_HOME}/ysoftman.omp.toml ] && mv -fv ${XDG_CONFIG_HOME}/ysoftman.omp.toml ${XDG_CONFIG_HOME}/ysoftman.omp.toml.bak
+[ -d ${XDG_CONFIG_HOME} ] && ln -sfv ${PWD}/oh-my-posh/ysoftman.omp.toml ${XDG_CONFIG_HOME}/ysoftman.omp.toml
--- installprezto.zsh
+++ installprezto.zsh
@@ -9,10 +9,17 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-ln -sfv ${PWD}/zpreztorc ${HOME}/.zprezto/runcoms/zpreztorc
-ln -sfv ${PWD}/zlogin ${HOME}/.zprezto/runcoms/zlogin
-ln -sfv ${PWD}/prompt_sorin_ysoftman_setup ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup
+[ -h ${HOME}/.zprezto/runcoms/zlogin ] && unlink ${HOME}/.zprezto/runcoms/zlogin
+[ -h ${HOME}/.zprezto/runcoms/zpreztorc ] && unlink ${HOME}/.zprezto/runcoms/zpreztorc
+[ -h ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup ] && unlink ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup
 
+[ -f ${HOME}/.zprezto/runcoms/zlogin ] && mv -fv ${HOME}/.zprezto/runcoms/zlogin ${HOME}/.zprezto/runcoms/zlogin.bak
+[ -f ${HOME}/.zprezto/runcoms/zpreztorc ] && mv -fv ${HOME}/.zprezto/runcoms/zpreztorc ${HOME}/.zprezto/runcoms/zpreztorc.bak
+[ -f ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup ] && mv -fv ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup.bak
+
+ln -sfv ${PWD}/prezto/zlogin ${HOME}/.zprezto/runcoms/zlogin
+ln -sfv ${PWD}/prezto/zpreztorc ${HOME}/.zprezto/runcoms/zpreztorc
+ln -sfv ${PWD}/prezto/prompt_sorin_ysoftman_setup ${HOME}/.zprezto/modules/prompt/functions/prompt_sorin_ysoftman_setup
 
 #echo 'source ${HOME}/.zprezto/init.zsh' >> ~/.zshrc
 source ${HOME}/.zprezto/init.zsh
