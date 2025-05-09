809e5501fa3dc16f13726f3e1066ceac9f761468
ysoftman <ysoftman@gmail.com>
2025-05-09 11:58:06 +0900

    add linking config in each installation

--- installstarship.sh
+++ installstarship.sh
@@ -6,3 +6,9 @@ if [[ $os_name == *"darwin"* ]]; then
 else
     curl -sS https://starship.rs/install.sh | sh
 fi
+
+export XDG_CONFIG_HOME="$HOME/.config"
+mkdir -p ${XDG_CONFIG_HOME}
+[ -h ${XDG_CONFIG_HOME}/starship.toml ] && unlink ${XDG_CONFIG_HOME}/starship.toml
+[ -f ${XDG_CONFIG_HOME}/starship.toml ] && mv -fv ${XDG_CONFIG_HOME}/starship.toml ${XDG_CONFIG_HOME}/starship.toml.bak
+[ -d ${XDG_CONFIG_HOME} ] && ln -sfv ${PWD}/starship/starship.toml ${XDG_CONFIG_HOME}/starship.toml
