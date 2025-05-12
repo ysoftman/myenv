afd52a291dc9e7b8858e13cad6f1620898cef1c2
ysoftman <ysoftman@gmail.com>
2025-05-12 11:59:21 +0900

    find ysoftman.github.io path

--- git_find_ysoftman_dirs.sh
+++ git_find_ysoftman_dirs.sh
@@ -4,11 +4,13 @@ git_find_ysoftman_dirs() {
     # fd 결과 파일들이 한줄로 한번에 처리되는데 이때 File name too long 에러가 발생한다.
     # IFS 를 newline 로해 파일 1개씩 처리되도록 해야 한다.
     IFS=$'\n'
-    gitdirs=$(fd '\.git$' --hidden --type d $HOME/workspace)
+    # --no-ignore 옵션이 없으면 ~/workspace/ysoftman.github.io > .gitignore > .git 이 설정되어 있어 디렉토리가 제외된다.
+    gitdirs=$(fd '\.git$' --hidden --no-ignore --type d $HOME/workspace)
     for item in ${gitdirs}; do
         out=$(git -C ${item} remote -v | rg -i -c "https://github.com/ysoftman/.*fetch" | awk '{print$1}')
         if [[ $out == 1 ]]; then
-            echo "$item" | sed 's#.git/$##'
+            # echo "$item" | sed 's#.git/$##'
+            echO $item
         fi
     done
     IFS=' '
