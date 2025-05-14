c8298ec923eb33d7eb0d7edde116aa7de882c328
ysoftman <ysoftman@gmail.com>
2025-05-14 21:14:36 +0900

    update installapp.sh

--- installapp.sh
+++ installapp.sh
@@ -12,47 +12,18 @@ fi
 # sudo rm -rf '/Applications/Spectacle.app'
 # sudo rm -rf '/Applications/Flux.app'
 
-if [ -e '/Applications/iTerm.app' ]; then
-    echo "file exists /Applications/iTerm.app"
-else
-    itversion="3_5_13"
-    wget https://iterm2.com/downloads/stable/iTerm2-${itversion}.zip
-    tar zxf iTerm2-${itversion}.zip
-    sudo cp -R "iTerm.app" /Applications
-    rm -rf iTerm*
-fi
+# 이제는 brew 로 앱들 설치할 수 있다.
+brew install iterm2 visual-studio-code rectangle firefox google-chrome
 
-if [ -e '/Applications/Visual Studio Code.app' ]; then
-    echo "file exists /Applications/Visual Studio Code.app"
-else
-    wget https://az764295.vo.msecnd.net/stable/f46c4c469d6e6d8c46f268d1553c5dc4b475840f/VSCode-darwin-stable.zip
-    # symbolic links 를 보존하기 위해 unzip 사용
-    unzip VSCode-darwin-stable.zip
-    sudo cp -R "Visual Studio Code.app" /Applications
-    rm -rf "Visual Studio Code.app" "VSCode-darwin-stable.zip"
-    # 최초 실행 후 ctrl+shift+p -> install 'code' command in PATH 실행
-fi
-
-if [ -e '/Applications/Google Chrome.app' ]; then
-    echo "file exists /Applications/Google Chrome.app"
-else
-    wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-    hdiutil mount googlechrome.dmg
-    sudo cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
-    hdiutil unmount "/Volumes/Google Chrome"
-    rm -rf "googlechrome.dmg"
-fi
-
-if [ -e '/Applications/Firefox.app' ]; then
-    echo "file exists /Applications/Firefox.app"
-else
-    ffversion="138.0.1"
-    wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/${ffversion}/mac/ko/Firefox%20${ffversion}.dmg
-    hdiutil mount "Firefox $ffversion.dmg"
-    sudo cp -R "/Volumes/Firefox/Firefox.app" /Applications
-    hdiutil unmount "/Volumes/Firefox"
-    rm -rf Firefox*
-fi
+# if [ -e '/Applications/Google Chrome.app' ]; then
+#     echo "file exists /Applications/Google Chrome.app"
+# else
+#     wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+#     hdiutil mount googlechrome.dmg
+#     sudo cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+#     hdiutil unmount "/Volumes/Google Chrome"
+#     rm -rf "googlechrome.dmg"
+# fi
 
 # 이제는 mac 자체에서 같은 기능을 제공하고 있어 별도 설치 하지 않는다
 # if [ -e '/Applications/Flux.app' ]; then
