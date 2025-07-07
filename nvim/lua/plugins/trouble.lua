6830e37a444d569438976d0ab89173cb5e769621
ysoftman <ysoftman@gmail.com>
2025-07-07 15:42:02 +0900

    add keys settings

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,22 +1,40 @@
 return {
   "folke/trouble.nvim",
   enabled = true,
+  cmd = "Trouble",
   opts = {
-    multiline = true,
     use_diagnostic_signs = true,
-    modes = {
-      diagnostic = {
-        auto_open = true,
-        auto_preview = true,
-      },
-    },
   },
   keys = {
     {
-      mode = { "n" },
-      "<leader>tr",
-      "<cmd>Trouble<cr>",
-      desc = "Trouble",
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
     },
   },
 }
