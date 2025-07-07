0b06c6469600e46cee3ecd17c3a6422e28fd0e70
ysoftman <ysoftman@gmail.com>
2025-07-07 15:32:08 +0900

    disable warning annotation

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,5 +1,22 @@
 return {
   "folke/trouble.nvim",
   enabled = true,
-  opts = { use_diagnostic_signs = true },
+  opts = {
+    multiline = true,
+    use_diagnostic_signs = true,
+    modes = {
+      diagnostic = {
+        auto_open = true,
+        auto_preview = true,
+      },
+    },
+  },
+  keys = {
+    {
+      mode = { "n" },
+      "<leader>tr",
+      "<cmd>Trouble<cr>",
+      desc = "Trouble",
+    },
+  },
 }
