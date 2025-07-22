b31e571c7d24dc4f056555a6f6e2a9d0f221cf7b
ysoftman <ysoftman@gmail.com>
2025-07-22 19:17:48 +0900

    update rustup installation

--- installrust.sh
+++ installrust.sh
@@ -11,8 +11,11 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # cargo, rust-analyzer 등이 제대로 동작하지 않는다면 다음과 같이 삭제 후 재설치한다.
 # rustup uninstall stable && rustup install stable
 # ~/.rustup/toolchains/stable-aarch64-apple-darwin/bin 에 설치
-rustup component add rust-analyzer
+rustup component add clippy rust-analyzer
 # rust toolchains들을 ~/.cargo/bin/ 에서 링크를 생성하고 업데이트한다.
+# cargo ⇒ rustup
+# cargo-clippy ⇒ rustup
+# cargo-fmt ⇒ rustup
 # rls ⇒ rustup
 # rust-analyzer ⇒ rustup
 # rust-gdb ⇒ rustup
@@ -21,5 +24,4 @@ rustup component add rust-analyzer
 # rustc ⇒ rustup
 # rustdoc ⇒ rustup
 # rustfmt ⇒ rustup
-# rustup
 rustup update
