0e8bf0cd7b7e944e306078a60baed75abd429c7c
ysoftman <ysoftman@gmail.com>
2025-08-15 13:01:25 +0900

    rename find_duplicated_packages and update functions

---
+++ find_duplicated_packages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source ${HOME}/workspace/myenv/colors.sh
+
+# go install, brew install 로 중복 설치된 패키지 찾기
+find_duplicated_packages_in_go_and_brew() {
+    # local brew_pkgs=()
+    # local go_pkgs=()
+    # # brew_pkgs+=($(brew list | sort))  # shellcheck 경고 발생
+    # while IFS= read -r line; do
+    #     brew_pkgs+=("$line")
+    # done < <(brew list | sort)
+    # # go_pkgs+=($(ls ${GOPATH}/bin | sort)) # shellcheck 경고 발생
+    # while IFS= read -r line; do
+    #     go_pkgs+=("$line")
+    # done < <(ls ${GOPATH}/bin | sort)
+    # IFS_BAK=$IFS
+    # IFS=$'\n'
+    # local cnt=0
+    # for b in "${brew_pkgs[@]}"; do
+    #     for g in "${go_pkgs[@]}"; do
+    #         if [[ "$b" == "$g" ]]; then
+    #             echo "${green}$b${reset_color} is duplicated package in go and brew"
+    #         fi
+    #     done
+    # done
+    # IFS=$IFS_BAK
+
+    # comm 3번째열(공통항목)으로 찾기
+    duplicated_pkgs=$(comm -12 <(brew list | sort) <(ls ${GOPATH}/bin | sort))
+    echo "${green}$duplicated_pkgs${reset_color} is duplicated package in go and brew"
+}
