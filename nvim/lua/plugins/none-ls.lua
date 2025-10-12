b81ba7238b3a0b4e44985e9d866d1eab17f15a9d
ysoftman <ysoftman@gmail.com>
2025-10-12 15:26:54 +0900

    add biome to none-ls plugins

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,20 @@
 return {
   {
-    "nvimtools/none-ls.nvim", -- for formatting, diagnostics, code-actions, completion, hover
+    -- https://github.com/nvimtools/none-ls.nvim
+    -- null-ls 유지보수 종료 후 개선된 버전
+    -- formatting, diagnostics, code-actions, completion, hover 등을 지원
+    "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
       opts.root_dir = opts.root_dir
         or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
       opts.sources = vim.list_extend(opts.sources or {}, {
-        -- Add or modify sources here
+        -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
         nls.builtins.formatting.fish_indent,
         nls.builtins.diagnostics.fish,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.prettier,
+        nls.builtins.formatting.biome,
         nls.builtins.formatting.shfmt.with({
           -- conform shfmt 보다 none-ls shfmt 설정이 적용된다.
           -- use 4spaces instead of tabs(default)
