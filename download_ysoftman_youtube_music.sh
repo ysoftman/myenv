447e765cf8c0a0de46bf611ce8fdcd0639e49b1b
ysoftman <ysoftman@gmail.com>
2025-10-15 13:21:12 +0900

    update youtube playlists loop

--- download_ysoftman_youtube_music.sh
+++ download_ysoftman_youtube_music.sh
@@ -3,32 +3,23 @@
 download_ysoftman_youtube_music() {
     local music_path="$HOME/Downloads/music"
     mkdir -p $music_path
-    # download-archive 파일에 이미 다운로드된 음악 hash 가 기록되어 이를 참고하면 중복 다운로드를 피할 수 있다.
-    # --restrict-filenames 옵션으로 디렉토리,파일명에 아스키만 사용하도로하면 한글이름이 다 빠져 사용하지 않는다.
-    yt-dlp "https://www.youtube.com/playlist?list=PLxZefZxz0kXuZSbU4AQsIiES32Ddqbwbe" \
-        --download-archive $music_path/download-archive \
-        --extract-audio --audio-format mp3 \
-        --audio-quality 0 \
-        --ignore-errors \
-        -o "$music_path/%(playlist_title)s/%(title)s.%(ext)s"
-    yt-dlp "https://www.youtube.com/playlist?list=PLxZefZxz0kXva-lM3tLP49r5zZQKC_w5C" \
-        --download-archive $music_path/download-archive \
-        --extract-audio --audio-format mp3 \
-        --audio-quality 0 \
-        --ignore-errors \
-        -o "$music_path/%(playlist_title)s/%(title)s.%(ext)s"
-    yt-dlp "https://www.youtube.com/playlist?list=PLxZefZxz0kXt-LqxE7I2IJERF8-t_8FUT" \
-        --download-archive $music_path/download-archive \
-        --extract-audio --audio-format mp3 \
-        --audio-quality 0 \
-        --ignore-errors \
-        -o "$music_path/%(playlist_title)s/%(title)s.%(ext)s"
-    yt-dlp "https://youtube.com/playlist?list=PLxZefZxz0kXt0zWX9cy0Gf5DcikcA4S01" \
-        --download-archive $music_path/download-archive \
-        --extract-audio --audio-format mp3 \
-        --audio-quality 0 \
-        --ignore-errors \
-        -o "$music_path/%(playlist_title)s/%(title)s.%(ext)s"
+
+    playlists=(
+        "https://www.youtube.com/playlist?list=PLxZefZxz0kXuZSbU4AQsIiES32Ddqbwbe"
+        "https://www.youtube.com/playlist?list=PLxZefZxz0kXva-lM3tLP49r5zZQKC_w5C"
+        "https://www.youtube.com/playlist?list=PLxZefZxz0kXt-LqxE7I2IJERF8-t_8FUT"
+        "https://www.youtube.com/playlist?list=PLxZefZxz0kXt0zWX9cy0Gf5DcikcA4S01"
+    )
+    for pl in "${playlists[@]}"; do
+        # download-archive 파일에 이미 다운로드된 음악 hash 가 기록되어 이를 참고하면 중복 다운로드를 피할 수 있다.
+        # --restrict-filenames 옵션으로 디렉토리,파일명에 아스키만 사용하도로하면 한글이름이 다 빠져 사용하지 않는다.
+        yt-dlp "$pl" \
+            --download-archive $music_path/download-archive \
+            --extract-audio --audio-format mp3 \
+            --audio-quality 0 \
+            --ignore-errors \
+            -o "$music_path/%(playlist_title)s/%(title)s.%(ext)s"
+    done
 }
 
 # 플레이 리스트 정보만 출력시
