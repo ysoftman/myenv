d40d10347b81807d96b3ab51c58c9cf5c1d5c532
ysoftman <ysoftman@gmail.com>
2026-01-12 18:15:15 +0900

    disable fff.nvim

--- nvim/lua/plugins/fff.lua
+++ nvim/lua/plugins/fff.lua
@@ -1,28 +1,29 @@
 return {
-  "dmtrKovalenko/fff.nvim",
-  build = function()
-    -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-    -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-    require("fff.download").download_or_build_binary()
-  end,
-  -- if you are using nixos
-  -- build = "nix run .#release",
-  opts = { -- (optional)
-    debug = {
-      enabled = true, -- we expect your collaboration at least during the beta
-      show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
-    },
-  },
-  -- No need to lazy-load with lazy.nvim.
-  -- This plugin initializes itself lazily.
-  lazy = false,
-  keys = {
-    {
-      "ff", -- try it if you didn't it is a banger keybinding for a picker
-      function()
-        require("fff").find_files()
-      end,
-      desc = "FFFind files",
-    },
-  },
+  -- termux 에서 동작하지 않음, telescope.nvim fzf-lua 가 있으니 fff.nvim 은 사용하지 않는다.
+  -- "dmtrKovalenko/fff.nvim",
+  -- build = function()
+  --   -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+  --   -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+  --   require("fff.download").download_or_build_binary()
+  -- end,
+  -- -- if you are using nixos
+  -- -- build = "nix run .#release",
+  -- opts = { -- (optional)
+  --   debug = {
+  --     enabled = true, -- we expect your collaboration at least during the beta
+  --     show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+  --   },
+  -- },
+  -- -- No need to lazy-load with lazy.nvim.
+  -- -- This plugin initializes itself lazily.
+  -- lazy = false,
+  -- keys = {
+  --   {
+  --     "ff", -- try it if you didn't it is a banger keybinding for a picker
+  --     function()
+  --       require("fff").find_files()
+  --     end,
+  --     desc = "FFFind files",
+  --   },
+  -- },
 }
