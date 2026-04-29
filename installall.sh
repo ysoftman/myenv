c62b04bbf816a733d5e659ff97e6308b162d34d8
ysoftman <ysoftman@gmail.com>
2026-04-29 20:49:48 +0900

    add installcodex.sh to installall.sh

--- installall.sh
+++ installall.sh
@@ -39,6 +39,7 @@ bash ./installgh.sh
 bash ./installemojicli.sh
 bash ./installcolima.sh
 bash ./installclaudecode.sh
+bash ./installcodex.sh
 bash ./installzellij_tmux_shim.sh
 if [[ $(command -v code) ]]; then
     bash ./installvscodesettings.sh
