4e4683559d137cd2c12e454fbb3f45f022ca5497
ysoftman <ysoftman@gmail.com>
2026-08-05 13:00:08 +0900

    set ws alias for worktree

--- installconfig.sh
+++ installconfig.sh
@@ -5,6 +5,7 @@ git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.ws worktree
 git config --global color.ui auto
 # git config --global core.editor vim
 git config --global core.editor nvim
