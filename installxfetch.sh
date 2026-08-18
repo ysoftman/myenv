9b292e2937255ae502bf8fced9b9827bc1e0bec9
ysoftman <ysoftman@gmail.com>
2026-08-18 21:57:01 +0900

    fix xfetch plugin install

--- installxfetch.sh
+++ installxfetch.sh
@@ -3,4 +3,4 @@
 # https://github.com/xfetch-cli/xfetch
 curl -fsSL https://raw.githubusercontent.com/xfetch-cli/xfetch/main/install.sh | bash
 
-plugin install animate-logo
+~/.local/bin/xfetch plugin install animate-logo
