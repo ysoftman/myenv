9a69a5f3b28f532b0058d900a9807f635e6a363b
ysoftman <ysoftman@gmail.com>
2026-09-17 18:07:34 +0900

    fix mac used-memory formula and show usage percent

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,14 +6,18 @@ mac_used_mem() {
     fi
     # vm_stat 를 한 번만 호출해 모든 값을 한 스냅샷에서 추출한다.
     # 여러 번 호출하면 값이 어긋나거나 호출당 비용이 누적된다.
-    vm_stat | awk '
+    # used = (anon - purgeable) + wired + compressed : Activity Monitor "Memory Used" 와 같은 공식
+    # anon - purgeable : 파일 캐시와 버려도 되는(purgeable) 캐시를 제외한 앱 점유 메모리 (App Memory)
+    # active 를 쓰면 파일 캐시가 섞이고 inactive 앱 메모리가 빠져 실제보다 작게 나온다
+    vm_stat | awk -v total="$(sysctl -n hw.memsize)" '
         /page size of/        { page = $8 }
         /Pages wired down/    { wired = $4; sub(/\./, "", wired) }
-        /Pages active/        { active = $3; sub(/\./, "", active) }
+        /Pages purgeable/     { purgeable = $3; sub(/\./, "", purgeable) }
+        /Anonymous pages/     { anon = $3; sub(/\./, "", anon) }
         /Pages occupied by compressor/ { compressed = $5; sub(/\./, "", compressed) }
         END {
-            used = (wired + active + compressed) * page / 1024 / 1024 / 1024
-            printf "%dGi used\n", used
+            used = (anon - purgeable + wired + compressed) * page
+            printf "%.1fGi used (%d%%)\n", used / 1024 / 1024 / 1024, used * 100 / total
         }
     '
 }
